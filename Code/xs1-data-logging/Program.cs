a6a7b967644fdb0a3152273396484334ff8dfecc
Daniel Kirstenpfad <daniel@sones.de>
2010-12-18 15:13:11 +0100

    added data logging tool which connects to a EzControl XS1 and writes the retrieved sensor and actor data to disk

---
+++ Code/xs1-data-logging/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace xs1_data_logging
+{
+    /// <summary>
+    /// this small tool logs the data from one or many ezcontrol xs1 devices
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region ConsoleOutputLogger
+            ConsoleOutputLogger.verbose = true;
+            ConsoleOutputLogger.writeLogfile = false;
+            #endregion
+
+            #region Logo
+            ConsoleOutputLogger.WriteLine("EzControl XS1 Data Logger "+System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            ConsoleOutputLogger.WriteLine("(C) 2010 Daniel Kirstenpfad - http://github.com/bietiekay/hacs");
+            #endregion 
+
+            if (args.Length == 0)
+            {
+                ConsoleOutputLogger.WriteLine("Syntax:");
+                ConsoleOutputLogger.WriteLine("        xs1-data-logging.exe <ip or cname of xs1>vc");
+
+                return;
+            }
+
+            ConsoleOutputLogger.writeLogfile = true;
+
+            StringBuilder sb = new StringBuilder();
+            byte[] buf = new byte[8192];
+
+
+            String HacsURL = "http://"+args[0]+"/control?callback=cname&cmd=subscribe&format=tsv";
+
+            while (true)
+            {
+                //Thread.Sleep(100);
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(HacsURL);
+
+                // execute the request
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    ConsoleOutputLogger.WriteLineToScreenOnly("XS1 successfully connected!");
+                }
+                // we will read data via the response stream
+                Stream resStream = response.GetResponseStream();
+
+                string tempString = null;
+                int count = 0;
+
+                do
+                {
+                    // fill the buffer with data
+                    count = resStream.Read(buf, 0, buf.Length);
+
+                    // make sure we read some data
+                    if (count != 0)
+                    {
+                        // translate from bytes to ASCII text
+                        tempString = Encoding.ASCII.GetString(buf, 0, count);
+
+                        // continue building the string
+                        ConsoleOutputLogger.WriteLine(tempString);
+                    }
+                }
+                while (count > 0); // any more data to read?
+            }
+        }
+    }
+}
---
+++ Code/xs1-data-logging/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xs1-data-logging")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("xs1-data-logging")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b331541a-3346-41e6-9baa-eb2a2f810568")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
