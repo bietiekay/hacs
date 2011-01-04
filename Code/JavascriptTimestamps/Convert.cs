3fa9d81f30d33e5890f74ab11c7bf2e8d0e6a224
Daniel Kirstenpfad <daniel@sones.de>
2011-01-04 12:29:05 +0100

    utc bugfix for chart display

--- Code/JavascriptTimestamps/Convert.cs
+++ Code/JavascriptTimestamps/Convert.cs
@@ -10,12 +10,12 @@ namespace JavaScriptTimeStampExtension
         public static Int64 JavaScriptTimestamp(this DateTime myDateTime)
         {
             DateTime unixstarted = new DateTime(1970, 1, 1);  // reference date
-            return (Convert.ToInt64((myDateTime - unixstarted).TotalSeconds * 1000));
+            return (Convert.ToInt64((myDateTime - unixstarted).TotalSeconds * 1000 ));
         }
 
         public static void ParseJavaScriptTimestamp(this DateTime myDateTime, Int64 JavaScriptTimestamp)
         {
-
+            throw new NotImplementedException();
         }
     }
 }
