53d25e27bdbf3694de8198d12f3b69e508af4290
Daniel Kirstenpfad <daniel@sones.de>
2011-03-06 14:19:03 +0100

    only a given timespan of data is now being exported to through REST

--- Code/xs1-data-logging/JSONHandlers/JSONData.cs
+++ Code/xs1-data-logging/JSONHandlers/JSONData.cs
@@ -20,14 +20,12 @@ namespace xs1_data_logging.JSONHandlers
         {
             sensor_data = sensor_data_storage;
         }
-
-
-
+		
         /// <summary>
         /// generates JSON dataset from sensor data
         /// </summary>
         /// <returns></returns>
-        public String GenerateDataJSONOutput(ObjectTypes DataType, String ObjectTypeName, String ObjectName)
+        public String GenerateDataJSONOutputWithoutInterpolation(ObjectTypes DataType, String ObjectTypeName, String ObjectName, DateTime StartDateTime, DateTime EndDateTime)
         {
             /* Example:
              * 
@@ -41,38 +39,133 @@ namespace xs1_data_logging.JSONHandlers
 
             Output.Append("{ label: '"+ObjectName+"', data: [");
             bool firstdataset = true;
-
+			UInt64 SerializerCounter = 0;
+			UInt64 OutputCounter = 0;
+			double LastValue = double.NaN;
+			
+			// TODO: there should be an appropriate caching algorithm in the sensor data... 
+									
             foreach (OnDiscAdress ondisc in sensor_data.InMemoryIndex)
             {
-                XS1_DataObject dataobject = new XS1_DataObject();
-
-                dataobject.Deserialize(sensor_data.Read(ondisc));
-
-                if (dataobject.Type == DataType)
-                {
-                    if (dataobject.TypeName == ObjectTypeName)
-                    {
-                        if (dataobject.Name == ObjectName)
-                        { 
-                            if (!firstdataset)
-                                Output.Append(",");
-                            else
-                                firstdataset = false;
-
-                            Output.Append("[");                           
-                            Output.Append(dataobject.Timecode.JavaScriptTimestamp());
-                            Output.Append(",");
-                            Output.Append(dataobject.Value.ToString().Replace(',', '.'));
-                            Output.Append("]");
-                        }
-                    }
-                }
+				if (ondisc.CreationTime >= StartDateTime.Ticks)
+				{
+					if (ondisc.CreationTime <= EndDateTime.Ticks)
+					{
+		                XS1_DataObject dataobject = new XS1_DataObject();
+		
+		                dataobject.Deserialize(sensor_data.Read(ondisc));
+						SerializerCounter++;
+												
+		                if (dataobject.Type == DataType)
+		                {
+		                    if (dataobject.TypeName == ObjectTypeName)
+		                    {
+		                        if (dataobject.Name == ObjectName)
+		                        { 
+		                            if (!firstdataset)
+		                                Output.Append(",");
+		                            else
+		                                firstdataset = false;
+		
+									if (LastValue == double.NaN)
+									{
+										LastValue = dataobject.Value;
+			                            Output.Append("[");                           
+			                            Output.Append(dataobject.Timecode.JavaScriptTimestamp());
+			                            Output.Append(",");
+			                            Output.Append(dataobject.Value.ToString().Replace(',', '.'));
+			                            Output.Append("]");
+										OutputCounter++;
+									}
+									else
+									{
+										if (LastValue != dataobject.Value)
+										{
+				                            Output.Append("[");                           
+				                            Output.Append(dataobject.Timecode.JavaScriptTimestamp());
+				                            Output.Append(",");
+				                            Output.Append(dataobject.Value.ToString().Replace(',', '.'));
+				                            Output.Append("]");
+											OutputCounter++;
+										}
+										LastValue = dataobject.Value;
+									}
+										
+		                        }
+		                    }
+		                }
+					}
+				}
             }
-
             Output.Append("]}");
-
+			
+			ConsoleOutputLogger.WriteLineToScreenOnly("Generated JSON Dataset with "+SerializerCounter+" Elements and outputted "+OutputCounter+" Elements.");
 
             return Output.ToString();
         }
+		
+		
+		/// <summary>
+        /// generates JSON dataset from sensor data
+        /// </summary>
+        /// <returns></returns>
+        public String GenerateDataJSONOutput(ObjectTypes DataType, String ObjectTypeName, String ObjectName, DateTime StartDateTime, DateTime EndDateTime)
+        {
+            /* Example:
+             * 
+             * {    label: 'Europe (EU27)',
+             *       data: [[1999, 3.0], [2000, 3.9], [2001, 2.0], [2002, 1.2], [2003, 1.3], [2004, 2.5], [2005, 2.0], [2006, 3.1], [2007, 2.9], [2008, 0.9]]
+             * }
+             * 
+             * */
+
+            StringBuilder Output = new StringBuilder();
+
+            Output.Append("{ label: '"+ObjectName+"', data: [");
+            bool firstdataset = true;
+			UInt64 SerializerCounter = 0;
+			
+			// TODO: there should be an appropriate caching algorithm in the sensor data... 
+									
+            foreach (OnDiscAdress ondisc in sensor_data.InMemoryIndex)
+            {
+				if (ondisc.CreationTime >= StartDateTime.Ticks)
+				{
+					if (ondisc.CreationTime <= EndDateTime.Ticks)
+					{
+		                XS1_DataObject dataobject = new XS1_DataObject();
+		
+		                dataobject.Deserialize(sensor_data.Read(ondisc));
+						SerializerCounter++;
+												
+		                if (dataobject.Type == DataType)
+		                {
+		                    if (dataobject.TypeName == ObjectTypeName)
+		                    {
+		                        if (dataobject.Name == ObjectName)
+		                        { 
+		                            if (!firstdataset)
+		                                Output.Append(",");
+		                            else
+		                                firstdataset = false;
+		
+			                            Output.Append("[");                           
+			                            Output.Append(dataobject.Timecode.JavaScriptTimestamp());
+			                            Output.Append(",");
+			                            Output.Append(dataobject.Value.ToString().Replace(',', '.'));
+			                            Output.Append("]");										
+		                        }
+		                    }
+		                }
+					}
+				}
+            }
+            Output.Append("]}");
+			
+			ConsoleOutputLogger.WriteLineToScreenOnly("Generated JSON Dataset with "+SerializerCounter+" Elements");
+
+            return Output.ToString();
+        }
+
     }
 }
