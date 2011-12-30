869e6d531a35374a3a9560acf179d1e848df8f92
Daniel Kirstenpfad <hacs@schrankmonster.de>
2011-12-30 20:20:33 +0100

    + added scripting timer configuration + started scripting timer configuration handling and timer handling

---
+++ Code/xs1-data-logging/ScriptingTimers/ScriptingTimerConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// TimerName DateTime_Start(YearDoesn'tMatter) TimerPeriodDurationDays Duration_Minutes_Start Duration_Minutes_End OperationMode SwitchName JitterYesNo MinimumOnTimeMinutes
+
+namespace xs1_data_logging
+{
+    public enum scripting_timer_operation_modes
+    {
+        OnOff
+    }
+
+
+    public class ScriptingTimerElement
+    {
+        public String TimerName;
+        public DateTime Start;
+        public DateTime End;    
+        public Int32 Duration_Start;
+        public Int32 Duration_End;
+        public scripting_timer_operation_modes OperationMode;
+        public String SwitchName;
+        public Boolean Jitter;
+        public Boolean MinimumOnTime;
+    }
+
+    public class ScriptingTimerConfiguration
+    {
+
+    }
+}
