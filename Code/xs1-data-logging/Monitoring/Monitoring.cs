3b04ba29d77febc6adb8d839aa4d95261e0f5475
Daniel Kirstenpfad <bietiekay@gmail.com>
2012-10-07 22:33:05 +0200

    service monitoring and switching actions thread added

---
+++ Code/xs1-data-logging/Monitoring/Monitoring.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net.Sockets;   // TCP-streaming
+using System.Threading;     // the sleeping part...
+using System.Text;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+namespace xs1_data_logging
+{
+	public class MonitoringThread
+	{
+		public bool running = true;
+		private ConsoleOutputLogger ConsoleOutputLogger;
+
+		public MonitoringThread(ConsoleOutputLogger COL)
+		{
+			ConsoleOutputLogger = COL;
+		}
+
+		// this is the service monitoring script
+		public void Run()
+        {
+			while(running)
+			{
+				try
+				{
+					while(running)
+					{
+						Thread.Sleep (100);
+					}
+				}
+				catch(Exception e)
+				{
+					ConsoleOutputLogger.WriteLine(e.Message);
+				}
+			}
+		}
+	}
+}
+
