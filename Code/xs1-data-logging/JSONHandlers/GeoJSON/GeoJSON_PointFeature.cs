f1da83233518ef113c726dbbc06e929bceb02b4c
Daniel Kirstenpfad <bietiekay@gmail.com>
2013-04-13 22:14:43 +0200

    /data/geolocation?name=xxx&lastentry=true is working - outputting the last location of the user

--- Code/xs1-data-logging/JSONHandlers/GeoJSON/GeoJSON_PointFeature.cs
+++ Code/xs1-data-logging/JSONHandlers/GeoJSON/GeoJSON_PointFeature.cs
@@ -7,12 +7,25 @@ namespace xs1_data_logging
 	public class GeoJSON_PointFeature_Properties
 	{
 		public string popupContent { get; set; }
+
+		public GeoJSON_PointFeature_Properties(String Content)
+		{
+			popupContent = Content;
+		}
 	}
 	
 	public class GeoJSON_PointFeature_Geometry
 	{
 		public string type { get; set; }
 		public List<double> coordinates { get; set; }
+
+		public GeoJSON_PointFeature_Geometry(double Latitude, double Longitude)
+		{
+			coordinates = new List<double>();
+			coordinates.Add(Latitude);
+			coordinates.Add(Longitude);
+			type = "Point";
+		}
 	}
 	
 	public class GeoJSON_PointFeature_Feature
@@ -20,12 +33,24 @@ namespace xs1_data_logging
 		public string type { get; set; }
 		public GeoJSON_PointFeature_Properties properties { get; set; }
 		public GeoJSON_PointFeature_Geometry geometry { get; set; }
+
+		public GeoJSON_PointFeature_Feature(double Latitude, double Longitude, String Content)
+		{
+			type = "Feature";
+			properties = new GeoJSON_PointFeature_Properties(Content);
+			geometry = new GeoJSON_PointFeature_Geometry(Latitude,Longitude);
+		}
 	}
 	
 	public class GeoJSON_PointFeature_Root
 	{
 		public string type { get; set; }
 		public List<GeoJSON_PointFeature_Feature> features { get; set; }
+
+		public GeoJSON_PointFeature_Root()
+		{
+			features = new List<GeoJSON_PointFeature_Feature>();
+		}
 	}
 }
 
--- Code/xs1-data-logging/JSONHandlers/GeoJSON/Geolocation.cs
+++ Code/xs1-data-logging/JSONHandlers/GeoJSON/Geolocation.cs
@@ -1,5 +1,8 @@
 using System;
 using sones.storage;
+using sones.Storage;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace xs1_data_logging
 {
@@ -14,14 +17,59 @@ namespace xs1_data_logging
 			ConsoleOutputLogger_ = Logger;
 		}
 
+		private GoogleLatitudeDataObject ReadFromCache(OnDiscAdress adress)
+		{
+			GoogleLatitudeDataObject dataobject = new GoogleLatitudeDataObject();
+			
+			object cacheditem = latitude_data.Cache.ReadFromCache(adress);
+			if (cacheditem == null)
+			{
+				// not found in cache, read from disk and add to cache
+				dataobject.Deserialize(latitude_data.Read(adress));
+				latitude_data.Cache.AddToCache(adress,dataobject);
+			}
+			else
+			{
+				// found in cache, take it...
+				dataobject = (GoogleLatitudeDataObject)cacheditem;
+			}
+			
+			return dataobject;
+		}
+
 		public String GenerateJSON_LastEntry(String ObjectName)
 		{
 			if (latitude_data == null)
 				return "";
 
-			// todo: output current location for ObjectName here !!!
+			GoogleLatitudeDataObject pLatitudeValue = null;
 
-			return "";
+			// todo: output current location for ObjectName here !!!
+			lock (latitude_data.InMemoryIndex)
+			{
+				foreach (OnDiscAdress ondisc in latitude_data.InMemoryIndex.Reverse<OnDiscAdress>())
+				{
+					GoogleLatitudeDataObject dataobject = ReadFromCache(ondisc);
+
+					if (dataobject.AccountName.ToUpper() == ObjectName.ToUpper())
+					{
+						pLatitudeValue = dataobject;
+						break;
+					}
+				}
+			}
+
+			if (pLatitudeValue != null)
+			{
+				GeoJSON_PointFeature_Root root = new GeoJSON_PointFeature_Root();
+
+				GeoJSON_PointFeature_Feature feature = new GeoJSON_PointFeature_Feature(pLatitudeValue.Latitude,pLatitudeValue.Longitude,pLatitudeValue.AccountName+"@"+pLatitudeValue.reverseGeocode);
+				root.features.Add(feature);
+
+				return JsonConvert.SerializeObject(root);
+			}
+			else
+				return "not found";
 		}
 	}
 }
